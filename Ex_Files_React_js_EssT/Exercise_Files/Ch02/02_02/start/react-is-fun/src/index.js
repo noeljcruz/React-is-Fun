7e25748b67a2550a3da6e88924ee663b455cb7a3
Noel Cruz <noel.j.cruz@outlook.com>
2020-02-17 21:34:11 -0800

    modified index.js to include class component and passing props.

--- Ex_Files_React_js_EssT/Exercise_Files/Ch02/02_02/start/react-is-fun/src/index.js
+++ Ex_Files_React_js_EssT/Exercise_Files/Ch02/02_02/start/react-is-fun/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-var style = {
-    backgroundColor: 'orange',
-    color: 'white',
-    fontFamily: 'Arial'
+class Message extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 style={{ color: this.props.color }}>
+                    {this.props.msg}
+                </h1>
+                <p>I'll check back in {this.props.minutes} minutes</p>
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
-    <div style={style}>
-        <h1 id="heading-element">Hello World!</h1>
-        <p>We're glad you're here!</p>
-    </div>,
+    <Message color="blue" msg="how are you?" minutes={5}/>,
     document.getElementById('root')
 )
